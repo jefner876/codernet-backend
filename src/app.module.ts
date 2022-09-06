f9627a264d34f0c4c641dbc844f44db21d5cad99
jefner876 <jefner876@hotmail.com>
2022-09-06 10:49:02 +0100

    connect mongodb cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
+import{MongooseModule} from '@nestjs/mongoose'
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.DATABASE_URL) ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
