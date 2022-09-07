97beb81a7ff69ee4900a2204f71a28b41ce7edda
jefner876 <jefner876@hotmail.com>
2022-09-07 15:13:16 +0100

    split test and dev databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomGeneralGateway } from './gateways/room-general.gateway';
 import { UsersModule } from './users/users.module';
-import{MongooseModule} from '@nestjs/mongoose'
-import * as dotenv from 'dotenv' 
-dotenv.config()
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+const ENV = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL),UsersModule ],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL), UsersModule],
   controllers: [AppController],
   providers: [AppService, RoomGeneralGateway],
 })
